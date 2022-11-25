6d9de84ec6b6dc440daa373ed525d518955ac2d4
Pokeball69 <jatinchauhan99999@gmail.com>
2022-11-25 11:45:01 +0530

    kutta blli

---
+++ index.py
@@ -0,0 +1,16 @@
+# from pydoc import ModuleScanner
+# print("hello")
+# print("2+2 = ",2+2)
+# print(ModuleScanner)
+##odd/even
+# num = int(input("enter value: "))
+# if(num%2) == 0:
+#  print("number is even")
+# else:
+#  print("number is odd")
+
+
+##new
+x = 4       
+y = "Sally" 
+print(len(y)+x)
